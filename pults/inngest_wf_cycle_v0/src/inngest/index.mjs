5118b2bb5a2cbcfe83dcc9e0153cc34f32f26741
Leryk1981 <s.myasoedov81@gmail.com>
2025-12-21 18:03:39 +0100

    flashslot: add experiment A/B/C runner + pult event

--- pults/inngest_wf_cycle_v0/src/inngest/index.mjs
+++ pults/inngest_wf_cycle_v0/src/inngest/index.mjs
@@ -3,10 +3,12 @@ import wfCycleSmoke from "./wf_cycle_smoke.mjs";
 import wfCycleFull from "./wf_cycle_full.mjs";
 import wfCycleExperiment from "./wf_cycle_experiment.mjs";
 import flashslotPublish from "./flashslot_publish.mjs";
+import flashslotExperiment from "./flashslot_experiment.mjs";
 
 export const functions = [
   wfCycleSmoke(inngest),
   wfCycleFull(inngest),
   wfCycleExperiment(inngest),
   flashslotPublish(inngest),
+  flashslotExperiment(inngest),
 ];
